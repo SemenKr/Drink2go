73b8e7e0654ade263efcb0247e9f1dd4faab5f3a
Семён Крекотун <samkrekotyn@gmail.com>
2022-10-07 00:02:30 +0300

    работа над футером мобильной версии

--- source/js/main.js
+++ source/js/main.js
@@ -1,17 +1,32 @@
 const swiper = new Swiper('.swiper', {
-	slidesPerView: 1,
-	spaceBetween: 10,
+		slidesPerView: 1,
+		spaceBetween: 10,
 
-	pagination: {
-		el: '.swiper-pagination',
-	},
+		pagination: {
+				el: '.swiper-pagination',
+		},
 
-	navigation: {
-		nextEl: '.swiper-button-next',
-		prevEl: '.swiper-button-prev',
-	},
+		navigation: {
+				nextEl: '.swiper-button-next',
+				prevEl: '.swiper-button-prev',
+		},
 
-	scrollbar: {
-		el: '.swiper-scrollbar',
-	},
+		scrollbar: {
+				el: '.swiper-scrollbar',
+		},
+});
+
+let navMain = document.querySelector('.main-header');
+let navToggle = document.querySelector('.main-header__toggle');
+
+navMain.classList.remove('main-header--nojs');
+navMain.classList.add('main-header--menu_closed');
+
+navToggle.addEventListener('click', function () {
+if (navMain.classList.contains('main-header--menu_closed')) {
+navMain.classList.remove('main-header--menu_closed');
+navMain.classList.add('main-header--menu_opened');
+} else {
+navMain.classList.remove('main-header--menu_opened');
+}
 });
